e9ccdc3551397f21eca431c4bfde86ee70e745ed
Fabio Burzigotti <fburzigo@redhat.com>
2023-06-01 14:28:42 +0200

    [k8s-support] - Adding support for target environments to the Intersmash annotation

---
+++ tools/intersmash-tools-core/src/test/java/org/jboss/intersmash/tools/junit5/EmptyExtensionContext.java
@@ -0,0 +1,2 @@
+package org.jboss.intersmash.tools.junit5;public class EmptyExtensionContext {
+}
---
+++ tools/intersmash-tools-core/src/test/java/org/jboss/intersmash/tools/junit5/IntersmashExecutionConditionTest.java
@@ -0,0 +1,12 @@
+package org.jboss.intersmash.tools.junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntersmashExecutionConditionTest {
+
+    @Test
+    void evaluateExecutionCondition() {
+    }
+}
